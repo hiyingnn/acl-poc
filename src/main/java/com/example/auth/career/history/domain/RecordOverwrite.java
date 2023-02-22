90a71ff4dde3b86177eeecaa6d7f0946117020c1
hiyingnn <r.ying98@gmail.com>
2023-02-22 10:51:56 +0800

    feat(facet-record-authz): refactor into authz1 and authz, set up controller/service for new model

--- src/main/java/com/example/auth/career/history/domain/RecordOverwrite.java
+++ src/main/java/com/example/auth/career/history/domain/RecordOverwrite.java
@@ -1,9 +1,10 @@
 package com.example.auth.career.history.domain;
 
-import com.example.auth.auth.RecordOverwriteAcl;
+import com.example.auth.authz.authzv1.recordoverwrite.RecordOverwriteAcl;
 
 import java.util.List;
 
 public interface RecordOverwrite {
+
     List<RecordOverwriteAcl> getRecordAcls();
 }
